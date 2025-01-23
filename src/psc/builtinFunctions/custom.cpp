c1ae55ecaf125df51214ce04305a21a9f4ecc3dd
brownnpc <deaddogfuneral@gmail.com>
2025-01-23 22:21:33 +0500

    phase 0

--- src/psc/builtinFunctions/custom.cpp
+++ src/psc/builtinFunctions/custom.cpp
@@ -8,17 +8,14 @@
 #include "psc/types/types.h"
 #include "psc/variable.h"
 PSC::BuiltinFnGyatt::BuiltinFnGyatt()
-    : Function("LENGTH", PSC::DataType::INTEGER)
+    : Function("Gyatt", PSC::DataType::STRING)
 {
-    parameters.emplace_back("String", PSC::DataType::STRING, false);
+
 }
 
 void PSC::BuiltinFnGyatt::run(PSC::Context &ctx) {
-    PSC::Variable *var = ctx.getVariable("String");
-    if (var == nullptr || var->type != PSC::DataType::STRING) std::abort();
 
-    int_t len = (int_t) var->get<PSC::String>().value.size();
-    auto ret = std::make_unique<PSC::Integer>(len);
+    auto ret = std::make_unique<PSC::String>("GYATT");
 
-    ctx.returnValue = std::make_unique<NodeResult>(std::move(ret), PSC::DataType::INTEGER);
+    ctx.returnValue = std::make_unique<NodeResult>(std::move(ret), PSC::DataType::STRING);
 }
